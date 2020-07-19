0b58fef080f1a933fede09cc11f95f991dd4cf2d
EvgeniaKomleva <komleva.1999@inbox.ru>
2020-07-19 19:01:40 +0300

    fix next page

--- script.py
+++ script.py
@@ -64,15 +64,19 @@ def base(myurl, auth_status):
 
 
         # блок перехода на следующую страницу
-
-        try:
-            page = driver.find_element_by_xpath(
-                '/html/body/div[6]/div/div/div/div[2]/div/div[4]/div/div[2]/div/div[8]/div/a')  # Для перехода на след страницу (кроме последней!)
-            print(page)
-            page.click()
-            driver.implicitly_wait(5)
-        except:
-            print('last page done')
+        page = driver.find_element_by_xpath(
+            '/html/body/div[6]/div/div/div/div[2]/div/div[4]/div/div[2]/div/div[8]/div/a')  # Для перехода на след страницу (кроме последней!)
+        print(page)
+        page.click()
+        driver.implicitly_wait(5)
+        # try:
+        #     page = driver.find_element_by_xpath(
+        #         '/html/body/div[6]/div/div/div/div[2]/div/div[4]/div/div[2]/div/div[8]/div/a')  # Для перехода на след страницу (кроме последней!)
+        #     print(page)
+        #     page.click()
+        #     driver.implicitly_wait(5)
+        # except:
+        #     print('last page done')
 
         all_vacansu = driver.find_elements_by_xpath(
             '//a[@class="bloko-link bloko-link_dimmed HH-VacancyResponsePopup-Link"]')
@@ -80,6 +84,23 @@ def base(myurl, auth_status):
         if len(all_vacansu) == 0:
             print('AAAAAAAAAAAAAAAAA')
             print(i)
+
+    for vacansu in all_vacansu:
+        j = j + 1  # Номер текущего резюме
+
+        try:
+            vacansu.click()
+            # driver.find_element_by_xpath('//input[@class="bloko-radio__input HH-VacancyResponsePopup-Resume-Radio HH-InconsistencyChecker-Radio"]')
+            # print("find resume ")
+            v_button = driver.find_element_by_xpath(
+                '/html/body/div[9]/div[1]/div/form/div[4]/div/button/span[1]')  # .click()
+            v_button.click()
+            print(v_button.text)
+            print("find otklic")
+            driver.implicitly_wait(50)
+        except:
+            k = k + 1
+            print("-" * k)
 
     #print(last_page)
     print("count_error {}".format(k))
@@ -91,5 +112,7 @@ if __name__ == '__main__':
     #url = 'https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&resume=55273996ff07e206ce0039ed1f5761364e5042&specialization=1&showClusters=true'
     #url = 'https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&resume=55273996ff07e206ce0039ed1f5761364e5042&specialization=1&only_with_salary=true&salary=220000&from=cluster_compensation&showClusters=false'
     #url = 'https://hh.ru/search/vacancy?clusters=true&specialization=1&resume=55273996ff07e206ce0039ed1f5761364e5042&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text=Python+%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA&from=suggest_post'
-    url = 'https://hh.ru/search/vacancy?resume=55273996ff07e206ce0039ed1f5761364e5042&from=resumelist'
+    #url = 'https://hh.ru/search/vacancy?resume=55273996ff07e206ce0039ed1f5761364e5042&from=resumelist'
+    #url = 'https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&search_field=name&text=data+scientist&experience=noExperience&from=cluster_experience&showClusters=true'
+    url = 'https://hh.ru/vacancies/data-scientist'
     base(url, auth_status)
